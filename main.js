5680dff9453afa85741c17e74a91fcc80793462b
samathaluca <41462391+samathaluca@users.noreply.github.com>
2019-11-06 16:43:42 +0000

    autocomplete directions

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-      function initMap() {
+ function initMap() {
         var directionsService = new google.maps.DirectionsService;
         var directionsDisplay = new google.maps.DirectionsRenderer;
         var map = new google.maps.Map(document.getElementById('map'), {
