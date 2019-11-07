4f4654baea2cad14ba92fe40dcb34efd6c44d56a
samathaluca <samanthaluca@aol.com>
2019-11-07 23:22:08 +0000

    update commit name

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBNne6TTHmVK0n_fL0ogNWkvNwDTdf9lsI&libraries=places&callback=initialize" async defer></script>
 <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 
+change comment
